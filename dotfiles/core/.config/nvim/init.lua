ee33d128ee0032645937844ef9c929337c6545ee
Caden Kruckeberg <204605905+CadenKruckeberg@users.noreply.github.com>
2026-07-05 21:14:32 -0500

    neovim yank highlight

--- dotfiles/core/.config/nvim/init.lua
+++ dotfiles/core/.config/nvim/init.lua
@@ -67,3 +67,9 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.treesitter.start()
   end,
 })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function() vim.hl.on_yank() end,
+})
